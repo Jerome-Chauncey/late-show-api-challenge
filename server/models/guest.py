dcfa7cff803b07880deea9bf584e07f8fda776e4
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-06-22 21:26:53 +0300

    creates guest model

---
+++ server/models/guest.py
@@ -0,0 +1,26 @@
+from config import db
+
+class Guest(db.Model):
+    __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    occupation = db.Column(db.String)
+    
+
+
+
+
+
+
+
+
+
+
+
+# Guest
+# id
+
+# name
+
+# occupation
