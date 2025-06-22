b0aaabd73e18e4cb87d03f945e46314f482c9000
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-06-22 21:02:39 +0300

    configures config.py, .env amd requirements.txt

---
+++ server/config.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+app.config.from_object(Config)
+app.json.compact = False
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
